2faaa0d7f7d3a0451c9259c770628551298de159
Theo <theo.sementa@neopixl.com>
2024-07-23 11:16:04 +0200

    Initial Commit

---
+++ NetworkTemplate/NetworkTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkTemplateApp.swift
+//  NetworkTemplate
+//
+//  Created by Theo Sementa on 23/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetworkTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
